4d12ca85c1260a80dddc4f38ce7023facdfcf726
krishnpratapKP <pratap.1010@outlook.com>
2018-03-20 21:29:23 +0530

    Location and Image Upload to the Server

--- app/src/main/java/com/example/krishnapratap/automatepayroll/LoginPage.java
+++ app/src/main/java/com/example/krishnapratap/automatepayroll/LoginPage.java
@@ -3,7 +3,6 @@ package com.example.krishnapratap.automatepayroll;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
 import android.location.Location;
 import android.net.Uri;
@@ -14,14 +13,13 @@ import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
 import static android.Manifest.permission.ACCESS_FINE_LOCATION;
-import static java.lang.System.exit;
-
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -32,25 +30,40 @@ import com.google.android.gms.location.LocationServices;
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.time.LocalTime;
 import java.util.Date;
 
 
-public class LoginPage extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks,GoogleApiClient.OnConnectionFailedListener,LocationListener{
+public class LoginPage extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
 
     private TextView mLongitude;
     private TextView mLatitude;
     private GoogleApiClient mGoogleApiClient;
+
     private LocationRequest mLocationRequest;
-    private static final long mInterval=1000*60*1;
+
+
+    private long mInterval;
+
+    private long mTakePicture;
+    private Location mLocation;
+    private static final long lTakePicture = 360000;
+
+    private long rTakePicture = 0;
+
     private String msLongitude;
     private String msLatitude;
+
     private String mCurrentPhotoPath;
     static final int REQUEST_IMAGE_CAPTURE = 1;
-    private  ImageView mImageView;
+    private ImageView mImageView;
     private TextView mUsername;
     private String sUsername;
     private String mId;
-    private  String mDateTime;
+    private String mDateTime;
+    SharedPreferences sp;
+    int rImage = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,23 +71,35 @@ public class LoginPage extends AppCompatActivity implements GoogleApiClient.Conn
         setContentView(R.layout.activity_login_page);
 
 
-        mImageView=(ImageView)findViewById(R.id.uImageView);
-        mUsername=(TextView)findViewById(R.id.tUsername);
+        mImageView = (ImageView) findViewById(R.id.uImageView);
+        mUsername = (TextView) findViewById(R.id.tUsername);
 
-        requestPermission();
+
         dispatchTakePicturInten();
 
+
+
+
+        mInterval = 1000 * 60 * 3;
         mGoogleApiClient = new GoogleApiClient.Builder(this)
                 .addApi(LocationServices.API)
                 .addConnectionCallbacks(this)
                 .addOnConnectionFailedListener(this)
                 .build();
-        SharedPreferences sp=getSharedPreferences("User_info",MODE_PRIVATE);
-        sUsername =sp.getString("username","N/A");
-        mId =sp.getString("Id","N/A");
+
+        sp = getSharedPreferences("User_info", MODE_PRIVATE);
+        sUsername = sp.getString("username", "N/A");
+        mId = sp.getString("Id", "N/A");
+
         mUsername.setText(sUsername);
-        mLatitude=(TextView)findViewById(R.id.latitude);
-        mLongitude=(TextView)findViewById(R.id.longitude);
+        mLatitude = (TextView) findViewById(R.id.latitude);
+        mLongitude = (TextView) findViewById(R.id.longitude);
+    }
+
+    @NonNull
+    private Date getsTime() {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm:ss");
+        return new Date();
     }
 
     @Override
@@ -91,25 +116,24 @@ public class LoginPage extends AppCompatActivity implements GoogleApiClient.Conn
     }
 
     @Override
-    public void onConnected( Bundle bundle) {
+    public void onConnected(Bundle bundle) {
 
-        mLocationRequest=LocationRequest.create();
+        mLocationRequest = LocationRequest.create();
         mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-        mLocationRequest.setInterval(mInterval);
+        mLocationRequest.setFastestInterval(mInterval);
 
-        if(ActivityCompat.checkSelfPermission(this,ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED&&ActivityCompat.checkSelfPermission(this,ACCESS_COARSE_LOCATION)!=PackageManager.PERMISSION_GRANTED){
+        if (ActivityCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             return;
         }
-        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient,mLocationRequest,this);
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
 
     }
 
     private void fileSize() {
-        File file=new File(mCurrentPhotoPath);
-        if(file.length()==0)
-        {
+        File file = new File(mCurrentPhotoPath);
+        if (file.length() == 0) {
 
-            Intent intent=new Intent(LoginPage.this,MainActivity.class);
+            Intent intent = new Intent(LoginPage.this, MainActivity.class);
             startActivity(intent);
 
         }
@@ -128,74 +152,107 @@ public class LoginPage extends AppCompatActivity implements GoogleApiClient.Conn
     @Override
     public void onLocationChanged(Location location) {
 
-        msLatitude=String.valueOf(location.getLatitude());
-        msLongitude=String.valueOf(location.getLongitude());
+        getLocation(location);
+        mLocation = location;
+        String mLocation = "location";
+        Date date = getsTime();
+        mTakePicture = date.getTime();
+        rTakePicture = date.getTime() - mTakePicture;
+
+
+        Log.v("Ch", mTakePicture + "    mT   " + rTakePicture + "    rT   ");
+        Background background = new Background();
+        background.execute(mLocation, mId, msLongitude, msLatitude);
+
+
+    }
+
+    private void getLocation(Location location) {
+        msLatitude = String.valueOf(location.getLatitude());
+        msLongitude = String.valueOf(location.getLongitude());
         mLatitude.setText(msLatitude);
         mLongitude.setText(msLongitude);
-        String mLocation="location";
-        Background background = new Background();
-        background.execute(mLocation,mId,msLongitude,msLatitude);
     }
 
-    private  void requestPermission(){
-        ActivityCompat.requestPermissions(this,new String[]{ACCESS_FINE_LOCATION},1);
-        ActivityCompat.requestPermissions(this,new String[]{ACCESS_COARSE_LOCATION},1);
+    private void requestPermission() {
+        ActivityCompat.requestPermissions(this, new String[]{ACCESS_FINE_LOCATION}, 1);
+        ActivityCompat.requestPermissions(this, new String[]{ACCESS_COARSE_LOCATION}, 1);
     }
 
-    private void dispatchTakePicturInten(){
-        Intent takePictureIntent= new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if(takePictureIntent.resolveActivity(getPackageManager())!=null)
-        {
-            File photoFile=null;
-            try{
-                photoFile=createImageFile();
-            }catch (IOException e){
+    private void dispatchTakePicturInten() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException e) {
                 e.printStackTrace();
             }
 
-            if(photoFile!=null)
-            {
-                Uri photoUri = FileProvider.getUriForFile(this,"com.example.krishnapratap.automatepayroll",photoFile);
-                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,photoUri);
-                startActivityForResult(takePictureIntent,REQUEST_IMAGE_CAPTURE);
+            if (photoFile != null) {
+                Uri photoUri = FileProvider.getUriForFile(this, "com.example.krishnapratap.automatepayroll", photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
             }
         }
     }
 
     private File createImageFile() throws IOException {
-        String timestamp =new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        mDateTime=timestamp;
-        String imageFileName = "JPEG_"+timestamp+"_";
+        String timestamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        mDateTime = timestamp;
+        String imageFileName = "JPEG_" + timestamp + "_";
         File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        File image = File.createTempFile(imageFileName,".jpg",storageDir);
+        File image = File.createTempFile(imageFileName, ".jpg", storageDir);
 
-        mCurrentPhotoPath=image.getAbsolutePath();
+        mCurrentPhotoPath = image.getAbsolutePath();
 
-        return  image;
+        return image;
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
         fileSize();
-        String timestamp=mDateTime;
-        Background background=new Background();
-        background.execute("Image",mId,mCurrentPhotoPath,timestamp,msLatitude,msLongitude);
+
+        String timestamp = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss").format(new Date());
+        Background background = new Background();
+        background.execute("Image", mId, mCurrentPhotoPath, timestamp, msLatitude, msLongitude);
         mImageView.setImageDrawable(Drawable.createFromPath(mCurrentPhotoPath));
+
     }
 
-    private String getDateTime()  {
-        return new SimpleDateFormat("dd-MM-YYYY/HH:mm:ss").format(new Date());
-    }
 
     @Override
     public void onBackPressed() {
 
-        if(sUsername!=null)
-        {
+        if (sUsername != null) {
             Intent startMain = new Intent(Intent.ACTION_MAIN);
             startMain.addCategory(Intent.CATEGORY_HOME);
             startMain.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             startActivity(startMain);
         }
+    }
+
+
+    public void getPictureLocation() {
+        getLocation(mLocation);
+        dispatchTakePicturInten();
+
+    }
+
+    public void logout(View view) {
+
+        SharedPreferences.Editor editor = sp.edit();
+
+        editor.clear();
+        editor.putInt("reference", 1);
+        editor.apply();
+        getPictureLocation();
+        Intent intent=new Intent(LoginPage.this,MainActivity.class);
+        startActivity(intent);
+        LoginPage.this.finish();
+
+
+
     }
 }
--- app/src/main/java/com/example/krishnapratap/automatepayroll/MainActivity.java
+++ app/src/main/java/com/example/krishnapratap/automatepayroll/MainActivity.java
@@ -3,12 +3,12 @@ package com.example.krishnapratap.automatepayroll;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -22,89 +22,86 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.util.ArrayList;
-import java.util.StringJoiner;
+
+import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText userText,passText;
+    EditText userText, passText;
     Button mLogin;
+    SharedPreferences sp;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        userText=(EditText)findViewById(R.id.username);
-        passText=(EditText)findViewById(R.id.password);
-        mLogin=(Button)findViewById(R.id.bLogin);
+        requestPermission();
+        userText = (EditText) findViewById(R.id.username);
+        passText = (EditText) findViewById(R.id.password);
+        mLogin = (Button) findViewById(R.id.bLogin);
         mLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                String user=userText.getText().toString();
-                String pass=passText.getText().toString();
-                String type="login";
-                Background background=new Background();
-                background.execute(type,user,pass);
+                String user = userText.getText().toString();
+                String pass = passText.getText().toString();
+                String type = "login";
+                Background background = new Background();
+                background.execute(type, user, pass);
             }
         });
     }
 
 
-    public class Background extends AsyncTask<String, Void, String[]> {
+     public class Background extends AsyncTask<String, Void, String[]> {
 
         private String mUsername;
         private String mId;
-        ArrayList<String> stringJoiner= new ArrayList<>();
+        ArrayList<String> stringJoiner = new ArrayList<>();
 
         @Override
         protected String[] doInBackground(String... strings) {
 
-            String type=strings[0];
-             mUsername = strings[1];
-            String login_url="http://192.168.43.242/login.php";
+            String type = strings[0];
+            mUsername = strings[1];
+            String login_url = "http://192.168.43.242/login.php";
 
-            if(type.equals("login")){
+            if (type.equals("login")) {
 
-                try{
+                try {
 
-                    String username=strings[1];
-                    String password=strings[2];
+                    String username = strings[1];
+                    String password = strings[2];
 
                     URL url = new URL(login_url);
-                    HttpURLConnection httpURLConnection =(HttpURLConnection)url.openConnection();
+                    HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
                     httpURLConnection.setRequestMethod("POST");
                     httpURLConnection.setDoOutput(true);
                     httpURLConnection.setDoInput(true);
-                    OutputStream outputStream=httpURLConnection.getOutputStream();
-                    BufferedWriter bufferedWriter=new BufferedWriter(new OutputStreamWriter(outputStream,"UTF-8"));
-                    String post_data=URLEncoder.encode("username","UTF-8")+"="+URLEncoder.encode(username,"UTF-8")+"&"+URLEncoder.encode("password","UTF-8")+"="+ URLEncoder.encode(password,"UTF-8")+"&"+type;
+                    OutputStream outputStream = httpURLConnection.getOutputStream();
+                    BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
+                    String post_data = URLEncoder.encode("username", "UTF-8") + "=" + URLEncoder.encode(username, "UTF-8") + "&" + URLEncoder.encode("password", "UTF-8") + "=" + URLEncoder.encode(password, "UTF-8") + "&" + type;
                     bufferedWriter.write(post_data);
                     bufferedWriter.flush();
                     bufferedWriter.close();
                     outputStream.close();
-                    InputStream inputStream=httpURLConnection.getInputStream();
-                    BufferedReader bufferedReader=new BufferedReader(new InputStreamReader(inputStream,"iso-8859-1"));
-                    String result="";
-                    String line="";
-                    int m=0;
+                    InputStream inputStream = httpURLConnection.getInputStream();
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "iso-8859-1"));
+                    String line;
 
-                    while ((line=bufferedReader.readLine())!=null){
-                       // result+=line;
+                    while ((line = bufferedReader.readLine()) != null) {
                         stringJoiner.add(line);
                     }
-                    String[] sSplit=stringJoiner.toString().split("<br>");
-                    sSplit[0]=sSplit[0].replace("[","");
-                    sSplit[sSplit.length-1]=sSplit[sSplit.length-1].replace("]","");
+                    String[] sSplit = stringJoiner.toString().split("<br>");
+                    sSplit[0] = sSplit[0].replace("[", "");
+                    sSplit[sSplit.length - 1] = sSplit[sSplit.length - 1].replace("]", "");
                     bufferedReader.close();
                     inputStream.close();
                     httpURLConnection.disconnect();
-                    if(sSplit.equals(""))
-                        return new String[]{"No connection "};
-                    else
-                        return sSplit;
 
-                } catch (MalformedURLException e) {
-                    e.printStackTrace();
+                    return sSplit;
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
@@ -120,33 +117,41 @@ public class MainActivity extends AppCompatActivity {
         }
 
         @Override
-        protected void onPostExecute(String []s) {
+        protected void onPostExecute(String[] s) {
 
-            mId=s[0];
+            mId = s[0];
 
 
-            if(s[s.length-1].equals("login_success")) {
-                SharedPreferences sp=getSharedPreferences("User_info",MODE_PRIVATE);
-                SharedPreferences.Editor editor=sp.edit();
-                editor.putString("username",mUsername);
-                editor.putString("Id",mId);
+            if (s[s.length - 1].equals("login_success")) {
+                SharedPreferences sp = getSharedPreferences("User_info", MODE_PRIVATE);
+                SharedPreferences.Editor editor = sp.edit();
+                editor.putString("username", mUsername);
+                editor.putString("Id", mId);
                 editor.apply();
 
 
-                Intent intent = new Intent(MainActivity.this,LoginPage.class);
-               startActivity(intent);
+                Intent intent = new Intent(MainActivity.this, LoginPage.class);
+
+                startActivity(intent);
             }
         }
     }
+
 
     @Override
     public void onBackPressed() {
 
 
-            Intent startMain = new Intent(Intent.ACTION_MAIN);
-            startMain.addCategory(Intent.CATEGORY_HOME);
-            startMain.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            startActivity(startMain);
+        Intent startMain = new Intent(Intent.ACTION_MAIN);
+        startMain.addCategory(Intent.CATEGORY_HOME);
+        startMain.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(startMain);
 
     }
+
+    private void requestPermission() {
+        ActivityCompat.requestPermissions(this, new String[]{ACCESS_FINE_LOCATION}, 1);
+        ActivityCompat.requestPermissions(this, new String[]{ACCESS_COARSE_LOCATION}, 1);
+    }
+
 }
